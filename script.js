a57dddb487caa7d7d9e8dd477d089c1fe072f059
arminasi <armanminasyanc@gmail.com>
2022-02-01 22:41:04 +0400

    first todo

---
+++ script.js
@@ -0,0 +1,51 @@
+const arr = [];
+const root = document.getElementById("root");
+const addButton = document.getElementById("addBtn");
+
+const mainContainer = document.createElement("div");
+mainContainer.className = "mainContainer";
+
+function newNode() {
+    let input = document.getElementById("input");
+    if(input.value === "") {
+        return;
+    } 
+    const mainContainer = document.createElement("div");
+    const incomingText = document.createElement("div");
+    const removeBtnDiv = document.createElement("button");
+
+    mainContainer.className = "mainContainer";
+    mainContainer.id = "mainContainer";
+    incomingText.className = "incomingText";
+    removeBtnDiv.className = "removeBtn";
+
+    let newTodo = document.createElement("p");
+    newTodo.innerText = input.value;
+    mainContainer.appendChild(incomingText);
+    incomingText.appendChild(newTodo);
+    root.appendChild(mainContainer);
+    input.value = "";
+    mainContainer.addEventListener("click", function() {
+        const COMPLETED_COLOR = "rgb(139, 202, 139)";
+        const isCompleted = mainContainer.style.background === COMPLETED_COLOR;
+        mainContainer.style.background = isCompleted ? "rgb(113, 150, 150)" : 'rgb(139, 202, 139)'
+    });
+    mainContainer.addEventListener("dblclick", function() {
+        mainContainer.remove();
+    })
+    mainContainer.addEventListener("mouseover", function() {
+        mainContainer.style.transform = "scale(0.95)"
+    })
+    mainContainer.addEventListener("mouseleave", function() {
+        mainContainer.style.transform = ""
+    })
+    input.addEventListener("keyup", function(event) {
+        if(event.keyCode === 13) {
+            newNode();
+        }
+    })
+}
+
+addButton.addEventListener("click", function(event) {
+    newNode();
+});
